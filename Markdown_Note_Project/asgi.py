939bfc3a838ee86268f4035571a6ee3abd2b011c
parisatajiki <parisatajiki7547@gmail.com>
2025-08-13 21:00:24 +0330

    start project and first app

---
+++ Markdown_Note_Project/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Markdown_Note_Project project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Markdown_Note_Project.settings')
+
+application = get_asgi_application()
